f94f178390fdb37899a354715ac2e1809bbd1614
troygilmore04 <103476955+troygilmore04@users.noreply.github.com>
2022-04-17 02:42:59 -0400

    init main

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./app.css";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
